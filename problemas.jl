a4e2fa31a8409ae223f1a238bcd499de11738c6a
janainacm <43250347+janainacm@users.noreply.github.com>
2019-10-04 14:14:53 -0300

    Update problemas.jl

--- problemas.jl
+++ problemas.jl
@@ -12,7 +12,7 @@ const ProblemList =
   EquacaoNL(x -> 1 / (1 + exp(-0.1x + 1)) - 0.5, 0.0, "Sigmoid"),
   EquacaoNL(x->ln(x^4+1)-3, -4.0, "Log neperiano"),
   EquacaoNL(x->sen(x)*cos(x), 1.0, "Seno produto Cosseno"),
-  EquacaoNL(x->((exp(x)+exp(-x))/2)^2)-44(x^2+1), pi/2, "Cosseno hiperbólico"),
+  EquacaoNL(x->(((exp(x)+exp(-x))/2)^2)-44(x^2+1), pi/2, "Cosseno hiperbólico"),
   EquacaoNL(x->exp((x^3-1)/(x^2+1)) -12.5, -2.0, "Exponencial"),
   EquacaoNL(x->(x-0.9)(x-1)(x-1.1), 1.05, "Função de grau 3"),
   EquacaoNL(x->(x*exp(x)-44)/(x^3+x^2+x+sqrt(11)), 1.25, "Exponencial dividida por terceiro grau"),
