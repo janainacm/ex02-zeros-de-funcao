c5fb6a40f3e55ab78cc0963a5a4db2df3bdb729d
janainacm <43250347+janainacm@users.noreply.github.com>
2019-10-05 18:54:13 -0300

    Update problemas.jl

--- problemas.jl
+++ problemas.jl
@@ -15,7 +15,6 @@ const ProblemList =
   EquacaoNL(x -> (x*exp(x)-44)/(x^3+x^2+x+sqrt(11)), 1.25, "Exponencial dividida por terceiro grau"),
   EquacaoNL(x -> exp(sin(4/53*x)), 0.002503, "Exponencial com seno"),
   EquacaoNL(x -> cos(sqrt(2x^2))*csc(x), -14.1333, "Cotangente de raiz quadrada"),
-  EquacaoNL(x -> sin(x)*cos(x), 1.0, "Seno produto Cosseno"),
   EquacaoNL(x -> (x-0.9)*(x-1)*(x-1.1), 1.05, "Função de grau 3"),
   EquacaoNL(x -> log(x^2+1)/exp(-x^2+0.5x), -1.0, "Log natural dividido por exponencial")
 ]
