90af72a145fc0d5add59a24c0a7bcfa8d8044482
janainacm <43250347+janainacm@users.noreply.github.com>
2019-10-05 19:01:42 -0300

    Update problemas.jl

--- problemas.jl
+++ problemas.jl
@@ -10,6 +10,6 @@ const ProblemList =
   EquacaoNL(x -> x^5 - 2^5, 1.0, "Grau 5"),
   EquacaoNL(x -> sin(x) + max(3 - x, 0)^2 + max(x - 3.2, 0)^2, 1.0, "π por sin"),
   EquacaoNL(x -> 1 / (1 + exp(-0.1x + 1)) - 0.5, 0.0, "Sigmoid"),
-  EquacaoNL(x -> log(x^4+1)-3, -4.0, "Log neperiano"),
-  EquacaoNL(x -> exp((2*x+1)/(x^2+1)) -12.5, -2.0, "Exponencial com fração no expoente")
+  EquacaoNL(x -> (x*exp(x)-44)/(x^3+x^2+x+sqrt(11)), 1.25, "Exponencial dividida por terceiro grau"),
+  EquacaoNL(x -> exp(sin(4/53*x)), 0.002503, "Exponencial com seno")
 ]
