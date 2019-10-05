fa3e31ff0f75d03b80e62eaa21ed2e5e18cba764
janainacm <43250347+janainacm@users.noreply.github.com>
2019-10-05 19:34:14 -0300

    Update problemas.jl

--- problemas.jl
+++ problemas.jl
@@ -1,4 +1,4 @@
 const ProblemList =
 [
-EquacaoNL(x -> log(exp(x^2-0.5)), -1.0, "Log natural dividido por exponencial")
+EquacaoNL(x -> log(x^2/exp(x-1))+exp(x+1), -0.333, "Log natural dividido por exponencial")
 ]
