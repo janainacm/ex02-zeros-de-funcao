85c0e65143996a7c5bf40f6d0902c70c95a7ae5e
Lucas Akio <44353304+Xedoux@users.noreply.github.com>
2019-10-07 12:06:12 -0300

    Update problemas.jl - Grupo do Lucas, Larissa e Dennis (#8)
    * Update problemas.jl
    
    * Update problemas.jl
    
    * Update problemas.jl
    
    * Update problemas.jl
    
    * Update problemas.jl
    
    * Update problemas.jl
    
    * Update problemas.jl
    
    * Update problemas.jl
    
    * Update problemas.jl
    
    * problemas-corrigidos.jl
    
    Em tese todos estão corretos.
    
    * Update problemas.jl
    
    * Update problemas.jl
    
    Todos
    
    * Update problemas.jl
    
    Até "x por log"
    
    * Update problemas.jl
    
    Três últimos
    
    * Update problemas.jl
    
    Dois últimos
    
    * Update problemas.jl
    
    Até "Cos e exp"
    
    * Update problemas.jl
    
    * Update problemas.jl
    
    * Update problemas.jl
    
    * Update problemas.jl
    
    * Update problemas.jl
    
    * problemas-finais-C
    
    Em tese é para todos passarem

--- problemas.jl
+++ problemas.jl
@@ -10,4 +10,14 @@ const ProblemList =
   EquacaoNL(x -> x^5 - 2^5, 1.0, "Grau 5"),
   EquacaoNL(x -> sin(x) + max(3 - x, 0)^2 + max(x - 3.2, 0)^2, 1.0, "π por sin"),
   EquacaoNL(x -> 1 / (1 + exp(-0.1x + 1)) - 0.5, 0.0, "Sigmoid"),
+  EquacaoNL(x -> x^6 - 48*x^4 + 1344*x^2 -4096, 0.0,"Grau 6"),
+  EquacaoNL(x -> sin(exp(x))+cos(exp(x)), 0.0,"sen + cos de exp"),
+  EquacaoNL(x -> log(x^2 + 5)/exp(sin(x))+x, 0.0,"Log exp e sen"),
+  EquacaoNL(x -> log(sin(x)^2), 0.0,"Log e sin"),
+  EquacaoNL(x -> sin(x - ℯ)/cosh(x), 0.0,"Sen por cosseno hiperbólico"),
+  EquacaoNL(x -> cos(exp(sin(x))), 0.0,"Cos e exp"),
+  EquacaoNL(x -> x/(log(x^2 + sin(x) + 2)) + 5/4, 3.0,"x por ln composta"),
+  EquacaoNL(x -> x*exp((sin(x)) + x*sin(x) + 1), 3.0, "soma tudo"),
+  EquacaoNL(x -> 1/(atan(x^2+1) + log(1/exp(x))), 0.0,"soma com inversas"),
+  EquacaoNL(x -> exp(x)*sin(x + π/2), 0.0,"Sen vezes exp")
 ]
