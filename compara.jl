57a57efd8692e796cdbba8aef6e9e9a69617efb8
janainacm <43250347+janainacm@users.noreply.github.com>
2019-10-18 19:00:07 -0300

    Update compara.jl

--- compara.jl
+++ compara.jl
@@ -5,7 +5,55 @@ include("modelo.jl")
 include("metodos.jl")
 include("problemas.jl")
 
-# incluir os métodos individuais aqui
+function newtonadaptado(enl :: EquacaoNL{T};
+                atol = √eps(T),
+                rtol = √eps(T),
+                max_iter = 10_000,
+                max_time = 30.0,
+                max_eval = 10_000,
+               ) where T <: AbstractFloat
+
+  f(x) = fun_val(enl, x)
+  g(x) = der_val(enl, x)
+  h(x) = seg_der_val(enl, x) // segunda derivada
+  x = enl.x₀
+  fx = f(x)
+  ϵ = atol + rtol * abs(fx)
+
+  resolvido = abs(fx) ≤ ϵ
+  start_time = time()
+  Δt = 0.0
+  iter = 0
+  excedido = Δt > max_time || iter ≥ max_iter || contador(enl) ≥ max_eval
+
+  status = :desconhecido
+  while !(resolvido || excedido)
+    gx = g(x)
+    hx = h(x)
+    if abs(gx) < atol
+      status = :falha
+      break
+    end
+    x -= fx * gx / ( gx^2 - fx * hx)
+    fx = f(x)
+    resolvido = abs(fx) ≤ ϵ
+    Δt = time() - start_time
+    iter += 1
+    excedido = Δt > max_time || iter ≥ max_iter || contador(enl) ≥ max_eval
+  end
+
+  if resolvido
+    status = :resolvido
+  elseif excedido
+    if Δt > max_time
+      status = :max_time
+    else
+      status = :max_iter
+    end
+  end
+
+  return x, fx, status, iter, Δt
+end
 
 function compara()
   # Colocar aqui a lista de métodos
